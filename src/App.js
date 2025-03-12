79c6943cc82a5d9a0c15138f8f3424a806298cd6
byebaji <best.goodx@gmail.com>
2025-03-12 16:06:02 +0200

    add faq, reviews, contact

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Faq } from './components/Faq/Faq';
+import { Contact } from './components/Contact/Contact';
+import { Reviews } from './components/Reviews/Reviews';
 
 function App() {
-  return (
-    <>
-      <Header />
-      {/* <Hero /> */}
-      {/* <Numbers /> */}
-      {/* <Search /> */}
-      {/* <Services /> */}
-      {/* <Faq /> */}
-      {/* <Reviews /> */}
-      <Footer />
-      {/* <Call /> */}
-      {/* <Registration /> */}
-      {/* <Thank /> */}
-      {/* <Record /> */}
-      {/* <Vaccination /> */}
-      {/* <Contact /> */}
-      {/* <Menu /> */}
-    </>
-  );
+	return (
+		<>
+			<Header />
+			{/* <Hero /> */}
+			{/* <Numbers /> */}
+			{/* <Search /> */}
+			{/* <Services /> */}
+			<Faq />
+			<Reviews />
+			<Footer />
+			{/* <Call /> */}
+			{/* <Registration /> */}
+			{/* <Thank /> */}
+			{/* <Record /> */}
+			{/* <Vaccination /> */}
+			<Contact />
+			{/* <Menu /> */}
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Contact.scss';
+
+export const Contact = () => {
+	(() => {
+		// const refs = {
+		// 	openModalBtn: document.querySelector('[data-contact-open]'),
+		// 	closeModalBtn: document.querySelector('[data-contact-close]'),
+		// 	modal: document.querySelector('[data-contact]'),
+		// 	backdrop: document.querySelector('.backdrop'),
+		// };
+		// refs.openModalBtn.addEventListener('click', toggleModal);
+		// refs.closeModalBtn.addEventListener('click', toggleModal);
+		// refs.backdrop.addEventListener('click', toggleModal);
+		// function toggleModal(e) {
+		// 	if (e.currentTarget === refs.backdrop && e.target !== e.currentTarget)
+		// 		return;
+		// 	refs.modal.classList.toggle('is-hidden');
+		// 	document.body.classList.toggle('no-scroll');
+		// }
+	})();
+	return (
+		<div data-contact className='backdrop is-hidden'>
+			<div className='contact'>
+				<form autocomplete='on' className='contact__form'>
+					<button data-contact-close className='contact__close'>
+						<svg className='contact__icon' width='24' height='24'>
+							<use href='./svg/icons.svg#close-square'></use>
+						</svg>
+					</button>
+					<h1 className='contact__title'>
+						Хочете, зателефонуємо Вам за 30 секунд?
+					</h1>
+					<input
+						type='tel'
+						name='user-tel'
+						placeholder='Ваш номер телефону'
+						className='contact__input'
+						required
+					/>
+					<button className='contact__btn'>Зателефонуйте</button>
+					<p className='contact__time'>
+						00:<span className='contact__minutes'>29</span>:99
+					</p>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default Contact;
