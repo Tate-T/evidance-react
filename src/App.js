562c9a277d60b66f606f07aeed3a71a291ca6311
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-03-19 21:03:41 +0200

    BrowserRoute

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 
-import { Call } from "./components/Call/Call";
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
-import { Hero } from "./components/Hero/Hero";
-import { Menu } from "./components/Menu/Menu";
-import { Registration } from "./components/Registration/Registration";
-import { Search } from "./components/Search/Search";
-import { Numbers } from "./components/Numbers/Numbers";
-import { Reviews } from "./components/Reviews/Reviews";
-import { Faq } from "./components/Faq/Faq";
-import { Contact } from "./components/Contact/Contact";
-import { Services } from "./components/Services/Services";
+import "./index.scss";
+import { DoctorsPage } from "./pages/DoctorsPage";
+import { MainPage } from "./pages/MainPage";
 
-
-function App() {
-	return (
-		<>
-			<Header />
-			<Hero />
-			<Numbers />
-			<Search />
-			<Services />
-			<Faq />
-			<Reviews />
-			<Footer />
-			<Call />
-			<Registration />
-			{/* <Thank />  */}
-			{/* <Record />  */}
-			{/* <Vaccination />  */}
-			<Contact />
-			<Menu />
-
-		</>
-	);
-}
-
-export default App;
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/doctors" element={<DoctorsPage />} />
+    </Routes>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
+import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/evidance-react">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
