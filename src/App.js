001f65182a1893624aa53646299ce0d3de08107b
K. L <121451594+madparrot69@users.noreply.github.com>
2025-03-19 19:59:06 +0200

    numbers section

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import { Footer } from './components/Footer/Footer';
-import { Header } from './components/Header/Header';
-import { Faq } from './components/Faq/Faq';
-import { Contact } from './components/Contact/Contact';
-import { Reviews } from './components/Reviews/Reviews';
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Faq } from "./components/Faq/Faq";
+import { Contact } from "./components/Contact/Contact";
+import { Reviews } from "./components/Reviews/Reviews";
+import { Numbers } from "./components/Numbers/Numbers";
 
 function App() {
-	return (
-		<>
-			<Header />
-			{/* <Hero /> */}
-			{/* <Numbers /> */}
-			{/* <Search /> */}
-			{/* <Services /> */}
-			<Faq />
-			<Reviews />
-			<Footer />
-			{/* <Call /> */}
-			{/* <Registration /> */}
-			{/* <Thank /> */}
-			{/* <Record /> */}
-			{/* <Vaccination /> */}
-			<Contact />
-			{/* <Menu /> */}
-		</>
-	);
+  return (
+    <>
+      <Header />
+
+      {/* <Hero /> */}
+      <Numbers />
+      {/* <Search /> */}
+      {/* <Services /> */}
+      <Faq />
+      <Reviews />
+      <Footer />
+      {/* <Call /> */}
+      {/* <Registration /> */}
+      {/* <Thank /> */}
+      {/* <Record /> */}
+      {/* <Vaccination /> */}
+      <Contact />
+      {/* <Menu /> */}
+    </>
+  );
 }
 
 export default App;
