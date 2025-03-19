9d4c0250900d200aa50c14ee1e35cb75c43f0ed4
KoruzhynetsAnna <121615437+KoruzhynetsAnna@users.noreply.github.com>
2025-03-19 20:20:11 +0200

    Services component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import { Footer } from './components/Footer/Footer';
-import { Header } from './components/Header/Header';
-import { Faq } from './components/Faq/Faq';
-import { Contact } from './components/Contact/Contact';
-import { Reviews } from './components/Reviews/Reviews';
+
+import { Call } from "./components/Call/Call";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Hero } from "./components/Hero/Hero";
+import { Menu } from "./components/Menu/Menu";
+import { Registration } from "./components/Registration/Registration";
+import { Search } from "./components/Search/Search";
+import { Numbers } from "./components/Numbers/Numbers";
+import { Reviews } from "./components/Reviews/Reviews";
+import { Faq } from "./components/Faq/Faq";
+import { Contact } from "./components/Contact/Contact";
+import { Services } from "./components/Services/Services";
+
 
 function App() {
 	return (
 		<>
 			<Header />
-			{/* <Hero /> */}
-			{/* <Numbers /> */}
-			{/* <Search /> */}
-			{/* <Services /> */}
+			<Hero />
+			<Numbers />
+			<Search />
+			<Services />
 			<Faq />
 			<Reviews />
 			<Footer />
-			{/* <Call /> */}
-			{/* <Registration /> */}
-			{/* <Thank /> */}
-			{/* <Record /> */}
-			{/* <Vaccination /> */}
+			<Call />
+			<Registration />
+			{/* <Thank />  */}
+			{/* <Record />  */}
+			{/* <Vaccination />  */}
 			<Contact />
-			{/* <Menu /> */}
+			<Menu />
+
 		</>
 	);
 }
